dddd5348be78a46f5673f04be613217a8810b49f
ColinChan520 <a1864874@adelaide.edu.au>
2023-03-18 15:25:59 +1030

    first commit

---
+++ Truckloads.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int numTrucks(int numCrates, int loadSize) {
+  if (numCrates <= loadSize) {
+    return 1;
+  }
+
+  else {
+    if (numCrates % 2 == 0) {
+      return numTrucks(numCrates / 2, loadSize) +
+             numTrucks(numCrates / 2, loadSize);
+    } else {
+      return numTrucks(numCrates / 2 + 1, loadSize) +
+             numTrucks(numCrates / 2, loadSize);
+    }
+  }
+}
